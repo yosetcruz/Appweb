1e7025bfe1ac76df264e601af1a4bbab15ddb7f3
yosetcruz <josephcruz7321@gmail.com>
2026-04-24 01:41:15 -0600

    Modifico boton inicial

--- APPweb.php
+++ APPweb.php
@@ -44,6 +44,11 @@ h1 {
     transform: scale(1.05);
 }
 
+.boton:disabled {
+    opacity: 0.5;
+    cursor: auto;
+    transform: none; 
+}
 /* SIDEBAR */
 .sidebar {
     position: fixed;
@@ -125,7 +130,7 @@ h1 {
 
 <h1>Análisis del CMS</h1>
 
-<button class="boton" onclick="habilitarMuestras()">
+<button id="btnMuestras" class="boton" onclick="habilitarMuestras()">
     Seleccionar muestra
 </button>
 
@@ -169,6 +174,7 @@ function habilitarMuestras() {
     habilitado = true;
 
     document.getElementById("sidebar").classList.add("abierto");
+    document.getElementById("btnMuestras").disabled = true;
 
     let todas = document.querySelectorAll(".muestra");
     todas.forEach(el => el.classList.add("activa"));
@@ -202,7 +208,20 @@ function cerrarModal() {
 /* CERRAR SIDEBAR CON CLIC DERECHO */
 document.addEventListener("contextmenu", function(e) {
     e.preventDefault();
-    document.getElementById("sidebar").classList.remove("abierto");
+
+    let modal = document.getElementById("modal");
+
+    if (modal.style.display !=="flex"){ 
+        document.getElementById("sidebar").classList.remove("abierto");
+
+        habilitado = false; 
+
+        document.getElementById("btnMuestras").disabled = false;
+
+        document.querySelectorAll(".muestra").forEach(el => {
+            el.classList.remove("activa");
+        });
+    }
 });
 
 </script>
